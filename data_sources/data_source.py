25ad3447cd98a597f43001f80414aab66f61d346
yuval <yuvalmund@gmail.com>
2023-06-03 11:30:09 +0300

    creating data source base class and kafka ds

---
+++ data_sources/data_source.py
@@ -0,0 +1,35 @@
+from abc import ABC, abstractmethod
+from typing import List, Dict
+
+import boto3
+from botocore.exceptions import NoCredentialsError
+import uuid
+import pandas as pd
+from io import BytesIO
+
+
+class DataSource(ABC):
+    def __init__(self, s3_bucket):
+        self.s3_bucket = s3_bucket
+
+    @abstractmethod
+    def read_data_into_s3(self, file_size):
+        pass
+
+    def write_to_s3(self, data: List[Dict]):
+        # Initialize S3 client
+        s3 = boto3.client('s3')
+
+        # Convert data into parquet
+        df = pd.DataFrame(data)
+        parquet_buffer = BytesIO()
+        df.to_parquet(parquet_buffer, engine='pyarrow')
+
+        try:
+            s3.put_object(
+                Body=parquet_buffer,
+                Bucket=self.s3_bucket,
+                Key=f'data_{str(uuid.uuid4())}.json'
+            )
+        except NoCredentialsError:
+            print("Failed to write to S3. Check your AWS credentials.")
