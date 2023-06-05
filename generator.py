dca970301b299c763e7fd6ce7d7f1d32351d5c35
Ran Dayan <randayan10@gmail.com>
2023-06-05 18:00:59 +0300

    Basic functionality of generating fake data for pii columns

--- generator.py
+++ generator.py
@@ -1,8 +1,16 @@
 import pandas as pd
 from faker import Faker
 
+tracker_dict = {}
+ID = ["id"]
+FULL_NAME = ["name", "full name", "full_name"]
+FIRST_NAME = ["first name", "first_name", "fname"]
+LAST_NAME = ["last name", "last_name", "surname", "lname"]
+
+
 class DataGenerator:
     data: pd.DataFrame
+
     def __init__(self, data):
         self.data = data
         self.fake = Faker()
@@ -17,16 +25,28 @@ class DataGenerator:
 
         return schema
 
-    def generate_data(self, pii_columns):
+    def generate_data(self, pii_columns: list[str]) -> pd.DataFrame:
+        num_of_rows = self.data.shape[0]
         generated_data = {}
-        for column, config in self.data.items():
-            if config['type'] == 'private':
-                generated_data[column] = [self.fake.name() for _ in range(len(self.data))]
-            elif config['type'] == 'challenge':
-                # Implement logic to challenge the specific column
-                pass
+        for column in self.data.columns.tolist():
+            if column in pii_columns:
+                # Randomize a value for the column
+                generated_data[column] = [self.generate_by_category(column, self.data[column].values[i])
+                                          for i in range(num_of_rows)]
             else:
-                # For other columns, generate random data based on config
-                pass
+                # Take the existing value of the column
+                generated_data[column] = self.data[column].values
+        return pd.DataFrame(generated_data)
 
-        return generated_data
+    def generate_by_category(self, column: str, org_val):
+        if org_val in tracker_dict:
+            return tracker_dict[org_val]
+        elif column.lower() in ID:
+            tracker_dict[org_val] = str(self.fake.pyint(0,999999999))
+        elif column.lower() in FULL_NAME:
+            tracker_dict[org_val] = self.fake.name()
+        elif column.lower() in FIRST_NAME:
+            tracker_dict[org_val] = self.fake.first_name()
+        elif column.lower() in LAST_NAME:
+            tracker_dict[org_val] = self.fake.last_name()
+        return tracker_dict[org_val]
