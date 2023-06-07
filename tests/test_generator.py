277679ddce2c55cb02579a2e0f399a440f81a517
Ran Dayan <randayan10@gmail.com>
2023-06-07 14:58:50 +0300

    1. Columns configuration is now set by yml 2. Column classes use kwargs to get all parameters

--- tests/test_generator.py
+++ tests/test_generator.py
@@ -1,40 +1,36 @@
 import pandas as pd
+import yaml
 
-from column import FirstNameColumn, LastNameColumn, FullStreet, GeneralIntColumn
 from generator import DataGenerator
+
+COLUMN_CONFIG_PATH = "../config/column_config-test_synthetic_data.yml"
 
 
 def test_generate_synth_data_with_pii_columns():
     original_data = \
         {
-            'fname': ["Ran", "Yuval", "Ran", "John", "Mike"],
-            'lname': ["Dayan", "Mund", "Dayan", "Johnson", "Tyson"],
-            'address': ["Hairus 5", "Herut 13", "Hairus 5", "Florentin 10", "Rotshild 11"],
+            'first_name': ["Ran", "Yuval", "Ran", "John", "Mike"],
+            'last_name': ["Dayan", "Mund", "Dayan", "Johnson", "Tyson"],
+            'full_street': ["Hairus 5", "Herut 13", "Hairus 5", "Florentin 10", "Rotshild 11"],
             'age': [28, 88, 28, 43, 46]
         }
-    columns_types_dict = \
-        {
-            'fname': FirstNameColumn,
-            'lname': LastNameColumn,
-            'address': FullStreet,
-            'age': GeneralIntColumn
-        }
-    df = pd.DataFrame(original_data)
-    pii_columns = ['fname', 'lname', 'address']
+    with open(COLUMN_CONFIG_PATH, 'r') as file:
+        column_config = yaml.safe_load(file)
 
-    dg = DataGenerator(df, columns_types_dict)
-    synth_data = dg.generate_data(pii_columns)
+    data_frame = pd.DataFrame(original_data)
+    data_generator = DataGenerator(data_frame, column_config)
+    synth_data = data_generator.generate_data()
 
-    assert type(synth_data) == pd.DataFrame,  "The generated synthetic data should be a Pandas DataFrame"
-    assert synth_data.shape == df.shape, "The shape of the synthetic data should match the original DataFrame"
+    assert type(synth_data) == pd.DataFrame, "The generated synthetic data should be a Pandas DataFrame"
+    assert synth_data.shape == data_frame.shape, "The shape of the synthetic data should match the original DataFrame"
 
     for column in synth_data.columns.tolist():
-        if column in pii_columns:
+        if column_config[column]['is_pii']:
             for i in range(synth_data.shape[0]):
-                assert synth_data[column].values[i] != df[column][i], f"Expected different values," \
-                                                                      f"got {df[column][i]} in both tables"
+                assert synth_data[column].values[i] != data_frame[column][i], f"Expected different values," \
+                                                                              f"got {data_frame[column][i]} in both tables"
         else:
             for i in range(synth_data.shape[0]):
-                assert synth_data[column].values[i] == df[column][i], f"Expected euqal values," \
-                                                                      f"got {synth_data[column].values[i]} and " \
-                                                                      f"{df[column][i]}"
+                assert synth_data[column].values[i] == data_frame[column][i], f"Expected euqal values," \
+                                                                              f"got {synth_data[column].values[i]} and " \
+                                                                              f"{data_frame[column][i]}"
