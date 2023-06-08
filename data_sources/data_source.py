0e110c88eb0b9290eb07985287dff486004a89e3
Ran Dayan <randayan10@gmail.com>
2023-06-08 15:21:44 +0300

    Add the ability to push files to Kafka, both Parquets and DFs

--- data_sources/data_source.py
+++ data_sources/data_source.py
@@ -1,10 +1,13 @@
+import uuid
 from abc import ABC, abstractmethod
+from functools import partial
+from io import BytesIO
+from multiprocessing import Pool
 from typing import List, Dict
 
-from develop.s3_handler import S3Handler
-import uuid
 import pandas as pd
-from io import BytesIO
+
+from develop.s3_handler import S3Handler
 
 
 class DataSource(ABC):
@@ -20,7 +23,32 @@ class DataSource(ABC):
     def create_synthetic_data(self, num_processes):
         pass
 
-    def write_to_s3(self, data: List[Dict], s3, divider_column):
+    def _write_df_to_data_source(self, df, **kwargs):
+        pass
+
+    def write_parquet_to_data_source(self, parquet_path, **kwargs):
+        df = pd.read_parquet(parquet_path, engine="pyarrow")
+        self._write_df_to_data_source(df)
+
+    def write_dfs_to_data_source(self, dfs, num_processes=1, **kwargs):
+        pool = Pool(num_processes)
+
+        try:
+            # Start multiple instances of the consumer function in the process pool
+            pool.map(partial(self._write_df_to_data_source, **kwargs), dfs)
+        except KeyboardInterrupt:
+            # Terminate the pool upon interrupt
+            pool.terminate()
+        finally:
+            # Close the pool
+            pool.close()
+            pool.join()
+
+    def write_parquets_to_data_source(self, parquet_paths, num_processes=1, **kwargs):
+        dfs = [pd.read_parquet(parquet_path, engine="pyarrow") for parquet_path in parquet_paths]
+        self.write_dfs_to_data_source(dfs, num_processes=num_processes, **kwargs)
+
+    def write_to_s3(self, data: List[Dict], s3, divider_column=None):
         s3_handler = S3Handler(bucket_name=self.s3_bucket, s3_client=s3, divider_column=divider_column)
         # Convert data into parquet
         df = pd.DataFrame(data)
@@ -28,4 +56,4 @@ class DataSource(ABC):
         df.to_parquet(parquet_buffer, engine="pyarrow")
 
         # Upload to s3
-        s3_handler.upload(f"data_{str(uuid.uuid4())}.json", parquet_buffer)
+        s3_handler.upload(f"data_{str(uuid.uuid4())}.parquet", parquet_buffer)
